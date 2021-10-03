f73cfb511ace5e726f796f568b3ade760738e9d1
Mateusz Lisiński <mateusz.lisinski01@gmail.com>
2021-10-03 13:37:00 +0200

    Final version

---
+++ WorkAndFun/src/main/java/pl/infoshare/FileActions.java
@@ -0,0 +1,42 @@
+package pl.infoshare;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+
+public class FileActions {
+
+    public static ArrayList<String[]> makeArrayFromFile(Path path) {
+        //initialize targeted array to fill and list of strings contains particular lines
+        ArrayList<String[]> arrayFromFile = new ArrayList<>();
+        String[] particularLines = new String[0];
+
+        //get particular lines from file by separating them by sign of new line (\n)
+        try {
+            particularLines = Files.readString(path).split("\n");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //fill array line by line, and separate each line into another list, by "," sign
+        for (int i = 0; i < particularLines.length; i++) {
+            String[] newString = particularLines[i].split(",");
+            arrayFromFile.add(particularLines[i].split(","));
+        }
+        return arrayFromFile;
+    }
+
+    public static void writeToFile(Path path, String... parts) throws IOException {
+        FileWriter fstream = new FileWriter(String.valueOf(path), true);
+        BufferedWriter out = new BufferedWriter(fstream);
+        out.newLine();
+
+        String lineToFill = String.join(",",parts);
+
+        out.write(lineToFill);
+        //close buffer writer
+        out.close();
+
+    }
+}
