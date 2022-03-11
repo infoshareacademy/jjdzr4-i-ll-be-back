40eeab3cc73d82aaa369a4a02e40db3215aece36
Patryk Granda <szadowek91@gmail.com>
2022-03-11 20:59:58 +0100

    added, works fine, TODO: center images

--- WorkAndFun/src/main/java/pl/infoshare/workandfun/WorkAndFunApplication.java
+++ WorkAndFun/src/main/java/pl/infoshare/workandfun/WorkAndFunApplication.java
@@ -1,8 +1,11 @@
 package pl.infoshare.workandfun;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 
+//@EnableAutoConfiguration(exclude = {ErrorMvcAutoConfiguration.class})
 @SpringBootApplication
 public class WorkAndFunApplication {
 
