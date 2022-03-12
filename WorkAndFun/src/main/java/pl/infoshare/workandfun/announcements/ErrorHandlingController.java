984d18c7b2ef051db639454e6d02a820bc9cc4b0
Patryk Granda <szadowek91@gmail.com>
2022-03-12 07:34:19 +0100

    corrected with suggestions + added error loggers! - suprise :D

--- WorkAndFun/src/main/java/pl/infoshare/workandfun/announcements/ErrorHandlingController.java
+++ WorkAndFun/src/main/java/pl/infoshare/workandfun/announcements/ErrorHandlingController.java
@@ -1,5 +1,7 @@
 package pl.infoshare.workandfun.announcements;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
@@ -11,6 +13,8 @@ import javax.servlet.http.HttpServletRequest;
 @Controller
 public class ErrorHandlingController implements ErrorController {
 
+    private static final Logger LOGGER = LogManager.getLogger(AnnouncementController.class);
+
 
     @RequestMapping("/error")
     public String handleError(HttpServletRequest request) {
@@ -18,16 +22,21 @@ public class ErrorHandlingController implements ErrorController {
         Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
 
         if (status != null) {
-            Integer statusCode = Integer.valueOf(status.toString());
+            int statusCode = Integer.parseInt(status.toString());
+            LOGGER.error("Sorry, can not open website");
 
             if (statusCode == HttpStatus.NOT_FOUND.value()) {
+                LOGGER.error("Page not found 404");
                 return "error-404";
             } else if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+                LOGGER.error("Server error");
                 return "error-500";
             } else if (statusCode == HttpStatus.FORBIDDEN.value()){
+                LOGGER.error("Someone tried enter to restricted area. User not logged in.");
                 return "error-403";
             }
         }
+        LOGGER.error("Other error, please contact with support: 666-999-666");
         return "error";
     }
 
