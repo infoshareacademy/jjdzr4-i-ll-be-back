2e6843003888ce99f3eff1d725745c92d18831fd
Patryk Granda <szadowek91@gmail.com>
2022-03-24 22:16:39 +0100

    WAF security enabled, TODO many http configs in WebSecurityConfigurerAdapter

--- WorkAndFun/src/main/java/pl/infoshare/workandfun/config/SecurityConfig.java
+++ WorkAndFun/src/main/java/pl/infoshare/workandfun/config/SecurityConfig.java
@@ -1,6 +1,8 @@
 package pl.infoshare.workandfun.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -9,23 +11,38 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                .antMatchers("/", "/css/**", "/img/**", "/js/**", "/sign-in", "/api/sign-in", "/sign-up").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .formLogin()
-                .loginPage("/sign-in")
-                .failureUrl("/sign-in?error")
-                .loginProcessingUrl("/api/sign-in")
-                .defaultSuccessUrl("/", true)
-                .and()
-                .logout()
-                .logoutUrl("/api/sign-out")
-                .logoutSuccessUrl("/")
-                .deleteCookies("JSESSIONID")
-                .invalidateHttpSession(true);
+    @Order(1)
+    @Configuration
+    public static class MSConfig extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http
+                    .authorizeRequests()
+                    .antMatchers("/", "/css/**", "/img/**", "/js/**", "/sign-in", "/api/sign-in", "/sign-up").permitAll()
+                    .anyRequest().authenticated()
+                    .and()
+                    .formLogin()
+                    .loginPage("/sign-in")
+                    .failureUrl("/sign-in?error")
+                    .loginProcessingUrl("/api/sign-in")
+                    .defaultSuccessUrl("/", true)
+                    .and()
+                    .logout()
+                    .logoutUrl("/api/sign-out")
+                    .logoutSuccessUrl("/")
+                    .deleteCookies("JSESSIONID")
+                    .invalidateHttpSession(true);
+        }
+    }
+
+    @Order(2)
+    @Configuration
+    public static class PGConfig extends WebSecurityConfigurerAdapter {
+        private static final String HAS_WAF_SERVICE_IPS = "hasIpAddress('localhost:8081')";
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http
+                    .csrf().disable().authorizeRequests().antMatchers(HttpMethod.POST, "/v1/**").access(HAS_WAF_SERVICE_IPS);
+        }
     }
 }
